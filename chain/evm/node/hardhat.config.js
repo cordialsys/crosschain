381607b70180273b6d5a89f9d6d4645281cffcb9
Conor Patrick <conor@cordialsystems.com>
2023-09-07 21:59:01 +0000

    add ci workflow

--- chain/evm/node/hardhat.config.js
+++ chain/evm/node/hardhat.config.js
@@ -1,11 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+var chainId = process.env.CHAIN_ID
+chainId = chainId || "2"
+
+console.log("CHAIN_ID:", chainId)
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
   networks: {
     hardhat: {
-      chainId: 1337,
+      chainId: parseInt(chainId),
       hardfork: 'london',
       baseFeePerGas: "10000000000",
     },
