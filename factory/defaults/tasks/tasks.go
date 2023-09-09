054e521b992bcbaad7fa867c70eccdaf8286d1ef
Conor Patrick <conor@cordialsystems.com>
2023-09-09 19:38:59 +0000

    rename chasm -> signer, first pass

--- factory/defaults/tasks/tasks.go
+++ factory/defaults/tasks/tasks.go
@@ -46,13 +46,9 @@ func GenerateTasks(
 			Chain: string(xc.ETH),
 			Allow: []string{
 				"ETH -> WETH.ETH",
-				"WETH.ETH", // for compatibility
 				"ArbETH -> WETH.ArbETH",
-				"WETH.ArbETH",
 				"MATIC -> WMATIC.MATIC",
-				"WMATIC.MATIC",
 				"BNB -> WBNB.BNB",
-				"WBNB",
 			},
 			Operations: []xc.TaskConfigOperation{
 				{
