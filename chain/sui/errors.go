e81e787ba31067a10cc3eb0e1a459bf4ddb7b574
Conor Patrick <conor@cordialsystems.com>
2023-11-10 18:53:23 +0000

    sui: add transaction failure case

--- chain/sui/errors.go
+++ chain/sui/errors.go
@@ -8,25 +8,11 @@ import (
 
 func CheckError(err error) xc.ClientError {
 	msg := strings.ToLower(err.Error())
-	// if strings.Contains(msg, "insufficient funds for gas * price + value") {
-	// 	return xc.NoBalanceForGas
-	// }
-	// if strings.Contains(msg, "insufficient funds for transfer") {
-	// 	return xc.NoBalance
-	// }
-	// if strings.Contains(msg, "transaction underpriced") ||
-	// 	strings.Contains(msg, "response body closed") ||
-	// 	strings.Contains(msg, "not found") ||
-	// 	strings.Contains(msg, "eof") ||
-	// 	strings.Contains(msg, "bad rpc url") {
-	// 	return xc.NetworkError
-	// }
-	// if strings.Contains(msg, "transaction already in block chain") ||
-	// 	strings.Contains(msg, "already known") {
-	// 	return xc.TransactionExists
-	// }
 	if strings.Contains(msg, "eof") {
 		return xc.NetworkError
+	}
+	if strings.Contains(msg, "transaction execution failed") {
+		return xc.TransactionFailure
 	}
 
 	return xc.UnknownError
