0a65c2f416617f541f56f91320ab8fd02f8c6a6b
Conor Patrick <conor@cordialsystems.com>
2024-08-10 10:53:20 -0400

    add testing around reuse between evm + evm-legacy

--- chain/evm_legacy/builder_test.go
+++ chain/evm_legacy/builder_test.go
@@ -1,12 +1,38 @@
-package evm_legacy
+package evm_legacy_test
 
 import (
+	"fmt"
+	"testing"
+
 	xc "github.com/cordialsys/crosschain"
+	"github.com/cordialsys/crosschain/chain/evm/builder"
+	"github.com/cordialsys/crosschain/chain/evm_legacy"
+	"github.com/test-go/testify/require"
 )
 
-func (s *CrosschainTestSuite) TestNewTxBuilder() {
-	require := s.Require()
-	builder, err := NewTxBuilder(&xc.ChainConfig{})
-	require.NoError(err)
-	require.NotNil(builder)
+func TestBuilderLegacyTransfer(t *testing.T) {
+	// EVM legacy re-uses the EVM builder, but uses a different tx-input.
+	// This ensures that the builder properly typecasts/converts to the evm input, avoiding any panic.
+	b, _ := evm_legacy.NewTxBuilder(&xc.ChainConfig{})
+
+	from := "0x724435CC1B2821362c2CD425F2744Bd7347bf299"
+	to := "0x3ad57b83B2E3dC5648F32e98e386935A9B10bb9F"
+	amount := xc.NewAmountBlockchainFromUint64(100)
+	input := evm_legacy.NewTxInput()
+
+	fmt.Println("--- ", input.GetDriver())
+	fmt.Printf("--- %T\n", input)
+
+	input.GasTipCap = builder.GweiToWei(evm_legacy.DefaultMaxTipCapGwei - 1)
+	trans, err := b.NewTransfer(xc.Address(from), xc.Address(to), amount, input)
+	require.NoError(t, err)
+	require.NotNil(t, trans)
+
+	trans, err = b.NewTokenTransfer(xc.Address(from), xc.Address(to), amount, input)
+	require.NoError(t, err)
+	require.NotNil(t, trans)
+
+	trans, err = b.NewNativeTransfer(xc.Address(from), xc.Address(to), amount, input)
+	require.NoError(t, err)
+	require.NotNil(t, trans)
 }
