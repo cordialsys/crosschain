69238121ed137d6956f7074c661b7cc795eec86e
Vlad Radu <vrares93@gmail.com>
2024-09-20 14:45:17 +0300

    Submit Transaction [Draft].

---
+++ chain/xrp/builder/builder_test.go
@@ -0,0 +1,55 @@
+package builder_test
+
+import (
+	"testing"
+
+	xc "github.com/cordialsys/crosschain"
+	"github.com/cordialsys/crosschain/chain/template/builder"
+	"github.com/cordialsys/crosschain/chain/template/tx_input"
+	"github.com/test-go/testify/require"
+)
+
+type TxInput = tx_input.TxInput
+
+func TestNewTxBuilder(t *testing.T) {
+
+	builder1, err := builder.NewTxBuilder(&xc.ChainConfig{})
+	require.NotNil(t, builder1)
+	require.EqualError(t, err, "not implemented")
+}
+
+func TestNewNativeTransfer(t *testing.T) {
+
+	builder, _ := builder.NewTxBuilder(&xc.ChainConfig{})
+	from := xc.Address("from")
+	to := xc.Address("to")
+	amount := xc.AmountBlockchain{}
+	input := &TxInput{}
+	tf, err := builder.(xc.TxTokenBuilder).NewNativeTransfer(from, to, amount, input)
+	require.Nil(t, tf)
+	require.EqualError(t, err, "not implemented")
+}
+
+func TestNewTokenTransfer(t *testing.T) {
+
+	builder1, _ := builder.NewTxBuilder(&xc.ChainConfig{})
+	from := xc.Address("from")
+	to := xc.Address("to")
+	amount := xc.AmountBlockchain{}
+	input := &TxInput{}
+	tf, err := builder1.(xc.TxTokenBuilder).NewTokenTransfer(from, to, amount, input)
+	require.Nil(t, tf)
+	require.EqualError(t, err, "not implemented")
+}
+
+func TestNewTransfer(t *testing.T) {
+
+	builder1, _ := builder.NewTxBuilder(&xc.ChainConfig{})
+	from := xc.Address("from")
+	to := xc.Address("to")
+	amount := xc.AmountBlockchain{}
+	input := &TxInput{}
+	tf, err := builder1.NewTransfer(from, to, amount, input)
+	require.Nil(t, tf)
+	require.EqualError(t, err, "not implemented")
+}
--- chain/xrp/tx_input/tx_input.go
+++ chain/xrp/tx_input/tx_input.go
@@ -1,16 +1,46 @@
 package tx_input
 
 import (
+	"encoding/base64"
+	"encoding/hex"
 	xc "github.com/cordialsys/crosschain"
+	"github.com/cordialsys/crosschain/chain/xrp/tx"
 	"github.com/cordialsys/crosschain/factory/drivers/registry"
 )
 
 // TxInput for Template
 type TxInput struct {
 	xc.TxInputEnvelope
+	XRPTx          tx.XRPTransaction
+	SerializeXRPTx string
+	Pubkey         []byte
+}
+
+func (input *TxInput) SetPublicKey(pubkey []byte) error {
+	input.Pubkey = pubkey
+	return nil
+}
+
+func (input *TxInput) SetPublicKeyFromStr(pubkeyStr string) error {
+	var err error
+	var pubkey []byte
+	if len(pubkeyStr) == 128 || len(pubkeyStr) == 130 {
+		pubkey, err = hex.DecodeString(pubkeyStr)
+		if err != nil {
+			return err
+		}
+	} else {
+		pubkey, err = base64.RawStdEncoding.DecodeString(pubkeyStr)
+		if err != nil {
+			return err
+		}
+	}
+	input.Pubkey = pubkey
+	return nil
 }
 
 var _ xc.TxInput = &TxInput{}
+var _ xc.TxInputWithPublicKey = &TxInput{}
 
 func init() {
 	registry.RegisterTxBaseInput(&TxInput{})
@@ -40,12 +70,10 @@ func (input *TxInput) SetGasFeePriority(other xc.GasFeePriority) error {
 
 func (input *TxInput) IndependentOf(other xc.TxInput) (independent bool) {
 	// are these two transactions independent (e.g. different sequences & utxos & expirations?)
-	// default false
 	return true
 }
 
 func (input *TxInput) SafeFromDoubleSend(others ...xc.TxInput) (safe bool) {
 	// safe from double send ?
-	// default false
 	return true
 }
