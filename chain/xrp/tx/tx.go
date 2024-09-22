0ff0be760a3d70be5d65423a9d34bad93ca96d77
Vlad Radu <vrares93@gmail.com>
2024-09-22 17:03:23 +0300

    Review fixes.

--- chain/xrp/tx/tx.go
+++ chain/xrp/tx/tx.go
@@ -3,7 +3,6 @@ package tx
 import (
 	"crypto/sha256"
 	"encoding/hex"
-	"encoding/json"
 	"errors"
 	"fmt"
 	binarycodec "github.com/xyield/xrpl-go/binary-codec"
@@ -18,16 +17,16 @@ const (
 type TransactionType string
 
 type XRPTransaction struct {
-	Account            xc.Address          `json:"Account,omitempty"`         // Sending account address
-	Amount             xc.AmountBlockchain `json:"Amount,omitempty"`          // Amount to deliver
-	Destination        xc.Address          `json:"Destination,omitempty"`     // Destination account
-	Fee                string              `json:"Fee,omitempty"`             // Optional fee (if none, XRPL calculates it)
-	Flags              int                 `json:"Flags"`                     // Flags for this transaction
-	LastLedgerSequence int                 `json:"LastLedgerSequence"`        // Optional last ledger sequence
-	Sequence           int                 `json:"Sequence,omitempty"`        // Sequence number of the account
-	SigningPubKey      string              `json:"SigningPubKey,omitempty"`   // Public key of the account
-	TransactionType    TransactionType     `json:"TransactionType,omitempty"` // Type of the transaction
-	TxnSignature       string              `json:"TxnSignature,omitempty"`    // Transaction signature
+	Account     xc.Address          `json:"Account,omitempty"`     // Sending account address
+	Amount      xc.AmountBlockchain `json:"Amount,omitempty"`      // Amount to deliver
+	Destination xc.Address          `json:"Destination,omitempty"` // Destination account
+	Fee         string              `json:"Fee,omitempty"`         // Optional fee (if none, XRPL calculates it)
+	Flags       int                 `json:"Flags"`                 // Flags for this transaction
+	//LastLedgerSequence int                 `json:"LastLedgerSequence"`        // Optional last ledger sequence
+	Sequence        int             `json:"Sequence,omitempty"`        // Sequence number of the account
+	SigningPubKey   string          `json:"SigningPubKey,omitempty"`   // Public key of the account
+	TransactionType TransactionType `json:"TransactionType,omitempty"` // Type of the transaction
+	TxnSignature    string          `json:"TxnSignature,omitempty"`    // Transaction signature
 	//AccountTxnID       string              `json:"AccountTxnID"`       // Hash of a previous transaction
 	//DeliverMin     *Amount `json:"DeliverMin,omitempty"`     // Optional minimum amount to be delivered
 	//DestinationTag *uint32 `json:"DestinationTag,omitempty"` // Optional destination tag
@@ -43,11 +42,11 @@ type XRPTransaction struct {
 
 // Tx for Template
 type Tx struct {
-	XRPTx             *XRPTransaction
-	SignPubKey        *string
-	SerialisedXRPTx   *string
-	SerialisedForSign []byte
-	InputSignature    []xc.TxSignature
+	XRPTx                *XRPTransaction
+	SignPubKey           []byte
+	EncodeForSigning     []byte
+	EncodeXRPTx          *string
+	TransactionSignature []xc.TxSignature
 }
 
 var _ xc.Tx = &Tx{}
@@ -63,11 +62,11 @@ func (tx Tx) Sighashes() ([]xc.TxDataToSign, error) {
 		return nil, errors.New("missing XRP transaction")
 	}
 
-	if tx.SerialisedForSign == nil {
+	if tx.EncodeForSigning == nil {
 		return nil, errors.New("missing serialised XRP transaction")
 	}
 
-	hash := sha256.Sum256(tx.SerialisedForSign)
+	hash := sha256.Sum256(tx.EncodeForSigning)
 
 	return []xc.TxDataToSign{hash[:]}, nil
 
@@ -78,7 +77,7 @@ func (tx *Tx) AddSignatures(signatures ...xc.TxSignature) error {
 	for _, sig := range signatures {
 		signatureHex := hex.EncodeToString(sig)
 		tx.XRPTx.TxnSignature = signatureHex
-		tx.InputSignature = append(tx.InputSignature, sig)
+		tx.TransactionSignature = append(tx.TransactionSignature, sig)
 	}
 
 	return nil
@@ -86,7 +85,7 @@ func (tx *Tx) AddSignatures(signatures ...xc.TxSignature) error {
 
 // GetSignatures returns back signatures, which may be used for signed-transaction broadcasting
 func (tx *Tx) GetSignatures() []xc.TxSignature {
-	return tx.InputSignature
+	return tx.TransactionSignature
 }
 
 // Serialize returns the serialized tx
@@ -95,47 +94,31 @@ func (tx Tx) Serialize() ([]byte, error) {
 		return []byte{}, errors.New("missing XRP transaction")
 	}
 
-	//tx.XRPTx.SigningPubKey = hex.EncodeToString(txInput.Pubkey) // TODO: Possible this need to be added after signing it.
-	tx.XRPTx.SigningPubKey = *tx.SignPubKey
+	xrpTx := tx.XRPTx
 
-	jsonData, err := json.Marshal(tx.XRPTx)
+	result := make(map[string]interface{})
+	result["Account"] = string(xrpTx.Account)
+	result["Amount"] = xrpTx.Amount.String()
+	result["Destination"] = string(xrpTx.Destination)
+	result["Fee"] = xrpTx.Fee
+	result["Flags"] = xrpTx.Flags
+	//result["LastLedgerSequence"] = xrpTx.LastLedgerSequence
+	result["Sequence"] = xrpTx.Sequence
+	result["SigningPubKey"] = xrpTx.SigningPubKey
+	result["TransactionType"] = string(xrpTx.TransactionType)
+	result["TxnSignature"] = xrpTx.TxnSignature
+
+	encodeTx, err := binarycodec.Encode(result)
 	if err != nil {
-		return nil, errors.New("error marshalling struct")
+		return nil, fmt.Errorf("failed to serialise serialised XRP transaction: %v", err)
 	}
 
-	var result map[string]interface{}
-	err = json.Unmarshal(jsonData, &result)
-	if err != nil {
-		return nil, errors.New("error unmarshalling struct")
-	}
-
-	if sequence, ok := result["Sequence"].(float64); ok {
-		result["Sequence"] = int(sequence)
-	}
-
-	if flags, ok := result["Flags"].(float64); ok {
-		result["Flags"] = int(flags)
-	}
-
-	if lastLedgerIndex, ok := result["LastLedgerSequence"].(float64); ok {
-		result["LastLedgerSequence"] = int(lastLedgerIndex)
-	}
-
-	if txnSignature, ok := result["LastLedgerSequence"].(string); ok {
-		result["LastLedgerSequence"] = []byte(txnSignature)
-	}
-
-	serializedForSigning, err := binarycodec.Encode(result)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("serializedForSigning:", serializedForSigning)
-
-	decode, err := binarycodec.Decode(serializedForSigning)
+	// TODO: remove, only for test.
+	decode, err := binarycodec.Decode(encodeTx)
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println("decode:", decode)
 
-	return []byte(serializedForSigning), nil
+	return []byte(encodeTx), nil
 }
