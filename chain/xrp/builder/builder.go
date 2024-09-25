faac365695bf7ab3e51d941016d869690e1ef1ce
Vlad Radu <vrares93@gmail.com>
2024-09-25 15:28:54 +0300

    builder updates and unittests

--- chain/xrp/builder/builder.go
+++ chain/xrp/builder/builder.go
@@ -2,11 +2,11 @@ package builder
 
 import (
 	"encoding/hex"
-	"errors"
 	"fmt"
 	xc "github.com/cordialsys/crosschain"
-	"github.com/cordialsys/crosschain/chain/xrp/tx"
+	xrptx "github.com/cordialsys/crosschain/chain/xrp/tx"
 	xrptxinput "github.com/cordialsys/crosschain/chain/xrp/tx_input"
+	"github.com/sirupsen/logrus"
 	binarycodec "github.com/xyield/xrpl-go/binary-codec"
 )
 
@@ -28,50 +28,76 @@ func NewTxBuilder(cfgI xc.ITask) (xc.TxBuilder, error) {
 
 // NewTransfer creates a new transfer for an Asset, either native or token
 func (txBuilder TxBuilder) NewTransfer(from xc.Address, to xc.Address, amount xc.AmountBlockchain, input xc.TxInput) (xc.Tx, error) {
-	if _, ok := txBuilder.Asset.(*xc.TokenAssetConfig); ok {
+	switch asset := txBuilder.Asset.(type) {
+	case *xc.ChainConfig:
+		return txBuilder.NewNativeTransfer(from, to, amount, input)
+	case *xc.TokenAssetConfig:
 		return txBuilder.NewTokenTransfer(from, to, amount, input)
+	default:
+		contract := asset.GetContract()
+		logrus.WithFields(logrus.Fields{
+			"chain":      asset.GetChain().Chain,
+			"contract":   contract,
+			"asset_type": fmt.Sprintf("%T", asset),
+		}).Warn("new transfer for unknown asset type")
+		if contract != "" {
+			return txBuilder.NewTokenTransfer(from, to, amount, input)
+		} else {
+			return txBuilder.NewNativeTransfer(from, to, amount, input)
+		}
 	}
-	return txBuilder.NewNativeTransfer(from, to, amount, input)
 }
 
 // NewNativeTransfer creates a new transfer for a native asset
 func (txBuilder TxBuilder) NewNativeTransfer(from xc.Address, to xc.Address, amount xc.AmountBlockchain, input xc.TxInput) (xc.Tx, error) {
 	txInput := input.(*TxInput)
 
-	xrpTx := tx.XRPTransaction{
-		Account:     from,
-		Amount:      amount,
-		Destination: to,
-		Fee:         "12",
-		Flags:       0,
-		//LastLedgerSequence: txInput.LastLedgerSequence,
-		Sequence:        txInput.Sequence,
-		SigningPubKey:   hex.EncodeToString(txInput.PublicKey),
-		TransactionType: tx.PAYMENT,
+	//amountBlockchain := new(big.Int)
+	//amountBlockchain.SetString(amount.String(), 10)
+	//
+	//divisor := big.NewInt(1000)
+	//
+	//XRPAmountBlockchain := new(big.Int)
+	//XRPAmountBlockchain.Div(amountBlockchain, divisor)
+
+	XRPAmount := xrptx.AmountBlockchain{
+		StringValue: amount.String(),
+	}
+
+	xrpTx := xrptx.XRPTransaction{
+		Account:            from,
+		Amount:             XRPAmount,
+		Destination:        to,
+		Fee:                "10",
+		Flags:              0,
+		LastLedgerSequence: txInput.LastLedgerSequence,
+		Sequence:           txInput.Sequence,
+		SigningPubKey:      hex.EncodeToString(txInput.PublicKey),
+		TransactionType:    xrptx.PAYMENT,
 	}
 
 	result := make(map[string]interface{})
 	result["Account"] = string(xrpTx.Account)
-	result["Amount"] = xrpTx.Amount.String()
+	result["Amount"] = xrpTx.Amount.StringValue
 	result["Destination"] = string(xrpTx.Destination)
 	result["Fee"] = xrpTx.Fee
-	result["Flags"] = xrpTx.Flags
-	//result["LastLedgerSequence"] = xrpTx.LastLedgerSequence
-	result["Sequence"] = xrpTx.Sequence
+	result["Flags"] = int(xrpTx.Flags)
+	result["LastLedgerSequence"] = int(xrpTx.LastLedgerSequence)
+	result["Sequence"] = int(xrpTx.Sequence)
 	result["SigningPubKey"] = xrpTx.SigningPubKey
 	result["TransactionType"] = string(xrpTx.TransactionType)
 
-	encodeForSigning, err := binarycodec.EncodeForSigning(result)
+	encodeForSigningHex, err := binarycodec.EncodeForSigning(result)
 	if err != nil {
 		return nil, fmt.Errorf("failed to serialize transaction for signing %v", err)
 	}
 
-	encodeForSigningBytes, err := hex.DecodeString(encodeForSigning)
+	encodeForSigningBytes, err := hex.DecodeString(encodeForSigningHex)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create byte object from hex serialized transaction %v", err)
 	}
 
-	return &tx.Tx{
+	return &xrptx.Tx{
 		XRPTx:            &xrpTx,
 		SignPubKey:       txInput.PublicKey,
 		EncodeForSigning: encodeForSigningBytes,
@@ -80,5 +106,68 @@ func (txBuilder TxBuilder) NewNativeTransfer(from xc.Address, to xc.Address, amo
 
 // NewTokenTransfer creates a new transfer for a token asset
 func (txBuilder TxBuilder) NewTokenTransfer(from xc.Address, to xc.Address, amount xc.AmountBlockchain, input xc.TxInput) (xc.Tx, error) {
-	return nil, errors.New("not implemented")
+	asset := txBuilder.Asset
+	txInput := input.(*TxInput)
+
+	assetContract := asset.GetContract()
+	if assetContract == "" {
+		return nil, fmt.Errorf("asset does not have a contract")
+	}
+
+	tokenAsset, tokenContract, err := xrptx.ExtractAssetAndContract(assetContract)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse and extract asset and contract: %w", err)
+	}
+
+	XRPAmount := xrptx.AmountBlockchain{
+		AmountValue: &xrptx.Amount{
+			Currency: tokenAsset,
+			Issuer:   tokenContract,
+			Value:    amount.String(),
+		},
+	}
+
+	amountResult := make(map[string]interface{})
+	amountResult["currency"] = tokenAsset
+	amountResult["issuer"] = tokenContract
+	amountResult["value"] = amount.String()
+
+	xrpTx := xrptx.XRPTransaction{
+		Account:            from,
+		Amount:             XRPAmount,
+		Destination:        to,
+		Fee:                "10",
+		Flags:              0,
+		LastLedgerSequence: txInput.LastLedgerSequence,
+		Sequence:           txInput.Sequence,
+		SigningPubKey:      hex.EncodeToString(txInput.PublicKey),
+		TransactionType:    xrptx.PAYMENT,
+	}
+
+	result := make(map[string]interface{})
+	result["Account"] = string(xrpTx.Account)
+	result["Amount"] = amountResult
+	result["Destination"] = string(xrpTx.Destination)
+	result["Fee"] = xrpTx.Fee
+	result["Flags"] = int(xrpTx.Flags)
+	result["LastLedgerSequence"] = int(xrpTx.LastLedgerSequence)
+	result["Sequence"] = int(xrpTx.Sequence)
+	result["SigningPubKey"] = xrpTx.SigningPubKey
+	result["TransactionType"] = string(xrpTx.TransactionType)
+
+	encodeForSigningHex, err := binarycodec.EncodeForSigning(result)
+	if err != nil {
+		return nil, fmt.Errorf("failed to serialize transaction for signing %v", err)
+	}
+
+	encodeForSigningBytes, err := hex.DecodeString(encodeForSigningHex)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create byte object from hex serialized transaction %v", err)
+	}
+
+	return &xrptx.Tx{
+		XRPTx:            &xrpTx,
+		SignPubKey:       txInput.PublicKey,
+		EncodeForSigning: encodeForSigningBytes,
+	}, nil
 }
