e1198006d49800097603c9e6b337144c945a4e24
Conor Patrick <conor@cordialsystems.com>
2025-03-27 13:27:19 -0400

    add ZETA, BAND

--- chain/cosmos/address/address.go
+++ chain/cosmos/address/address.go
@@ -27,21 +27,6 @@ func (ab AddressBuilder) GetAddressFromPublicKey(publicKeyBytes []byte) (xc.Addr
 		return "", fmt.Errorf("address cannot be empty")
 	}
 
-	// err := sdk.VerifyAddressFormat(rawAddress)
-	// if err != nil {
-	// 	return xc.Address(""), err
-	// }
 	bech32Addr, err := sdk.Bech32ifyAddressBytes(ab.Asset.ChainPrefix, rawAddress)
 	return xc.Address(bech32Addr), err
-}
-
-// GetAllPossibleAddressesFromPublicKey returns all PossubleAddress(es) given a public key
-func (ab AddressBuilder) GetAllPossibleAddressesFromPublicKey(publicKeyBytes []byte) ([]xc.PossibleAddress, error) {
-	address, err := ab.GetAddressFromPublicKey(publicKeyBytes)
-	return []xc.PossibleAddress{
-		{
-			Address: address,
-			Type:    xc.AddressTypeDefault,
-		},
-	}, err
 }
