84670732853892e29dc0169a4169389641571821
Conor Patrick <conor@cordialsystems.com>
2025-05-19 18:52:23 -0400

    kaspa: add support

--- chain/kaspa/tx_input/tx_input.go
+++ chain/kaspa/tx_input/tx_input.go
@@ -10,12 +10,21 @@ type TxInput struct {
 	xc.TxInputEnvelope
 	Address xc.Address `json:"address"`
 	Utxos   []Utxo     `json:"utxo"`
+	// The estimated fee
+	FeePerGram xc.AmountBlockchain `json:"fee_per_gram"`
+	Mass       uint64              `json:"mass"`
+	// we also include the network minimum fee, as the estimated fee may be lower than this
+	MinFee xc.AmountBlockchain `json:"min_fee"`
 }
 
 type Utxo struct {
 	TransactionId string              `json:"transaction_id"`
 	Index         int                 `json:"index"`
 	Amount        xc.AmountBlockchain `json:"amount"`
+}
+
+func (utxo *Utxo) Equals(other *Utxo) bool {
+	return utxo.TransactionId == other.TransactionId && utxo.Index == other.Index
 }
 
 var _ xc.TxInput = &TxInput{}
@@ -41,23 +50,54 @@ func (input *TxInput) SetGasFeePriority(other xc.GasFeePriority) error {
 	if err != nil {
 		return err
 	}
-	// multiply the gas price using the default, or apply a strategy according to the enum
-	_ = multiplier
+	// apply it to the mass or the min fee.
+	// The min fee is often just '1', which isn't good for integer math.
+	// So we apply the multiplier to the mass.
+	mass := uint64(float64(input.Mass) * multiplier.InexactFloat64())
+	input.Mass = mass
+
 	return nil
 }
 
 func (input *TxInput) GetFeeLimit() (xc.AmountBlockchain, xc.ContractAddress) {
-	// get the max possible fee that could be spent on this transaction
-	return xc.NewAmountBlockchainFromUint64(0), ""
+	mass := xc.NewAmountBlockchainFromUint64(input.Mass)
+	feeEstimate := mass.Mul(&input.FeePerGram)
+
+	if feeEstimate.Cmp(&input.MinFee) < 0 {
+		return input.MinFee, ""
+	}
+	return feeEstimate, ""
 }
 
-func (input *TxInput) IndependentOf(other xc.TxInput) (independent bool) {
-	// are these two transactions independent (e.g. different sequences & utxos & expirations?)
-	// default false
-	return
+func (input *TxInput) IndependentOf(otherI xc.TxInput) (independent bool) {
+	other, ok := otherI.(*TxInput)
+	if ok {
+		for _, utxo := range input.Utxos {
+			for _, otherUtxo := range other.Utxos {
+				if utxo.Equals(&otherUtxo) {
+					// not independent, spending the same utxo
+					return false
+				}
+			}
+		}
+		return true
+	}
+	return false
 }
 func (input *TxInput) SafeFromDoubleSend(others ...xc.TxInput) (safe bool) {
-	// safe from double send ?
-	// default false
-	return
+	if !xc.SameTxInputTypes(input, others...) {
+		return false
+	}
+	// safe from double send only if spending the same utxo
+	for _, other := range others {
+		other, ok := other.(*TxInput)
+		if !ok {
+			return false
+		}
+		if input.IndependentOf(other) {
+			return false
+		}
+	}
+	// all share the a dependency, can't double send
+	return true
 }
