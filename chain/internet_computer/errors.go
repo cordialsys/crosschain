4288e12dd86134e789597c4e9be0bc5d541e83a7
PawelBis <24803993+PawelBis@users.noreply.github.com>
2025-07-31 12:39:54 +0200

    Implement chains/internet_computer/errors

--- chain/internet_computer/errors.go
+++ chain/internet_computer/errors.go
@@ -1,7 +1,34 @@
 package newchain
 
-import "github.com/cordialsys/crosschain/client/errors"
+import (
+	"strings"
+
+	icperrors "github.com/cordialsys/crosschain/chain/internet_computer/client/types/errors"
+	"github.com/cordialsys/crosschain/client/errors"
+)
 
 func CheckError(err error) errors.Status {
+	errMsg := err.Error()
+	if strings.Contains(errMsg, icperrors.CreatedInFutureError) {
+		return errors.TransactionFailure
+	}
+	if strings.Contains(errMsg, icperrors.TransactionTooOldError) {
+		return errors.TransactionFailure
+	}
+	if strings.Contains(errMsg, icperrors.BadFeeError) {
+		return errors.TransactionFailure
+	}
+
+	if strings.Contains(errMsg, icperrors.InsufficientFundsError) {
+		return errors.NoBalance
+	}
+	if strings.Contains(errMsg, icperrors.TransactionDuplicateError) {
+		return errors.TransactionExists
+	}
+
+	if strings.Contains(errMsg, icperrors.UnknownError) {
+		return errors.UnknownError
+	}
+
 	return ""
 }
