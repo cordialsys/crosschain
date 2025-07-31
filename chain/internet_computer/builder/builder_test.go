00a1e2e35584e4c27e52b62da652395cc446dadc
PawelBis <24803993+PawelBis@users.noreply.github.com>
2025-07-31 14:07:03 +0200

    Add internet_protocol tx hash test

--- chain/internet_computer/builder/builder_test.go
+++ chain/internet_computer/builder/builder_test.go
@@ -5,36 +5,37 @@ import (
 
 	xc "github.com/cordialsys/crosschain"
 	"github.com/cordialsys/crosschain/builder/buildertest"
-	"github.com/cordialsys/crosschain/chain/template/builder"
-	"github.com/cordialsys/crosschain/chain/template/tx_input"
+	"github.com/cordialsys/crosschain/chain/internet_computer/builder"
+	"github.com/cordialsys/crosschain/chain/internet_computer/tx_input"
 	"github.com/stretchr/testify/require"
 )
 
 type TxInput = tx_input.TxInput
 
 func TestNewTxBuilder(t *testing.T) {
-	builder1, err := builder.NewTxBuilder(xc.NewChainConfig("XYZ").Base())
-	require.NotNil(t, builder1)
-	require.EqualError(t, err, "not implemented")
+	builder, err := builder.NewTxBuilder(xc.NewChainConfig("ICP").Base())
+	require.NotNil(t, builder)
+	require.NoError(t, err)
 }
 
-func TestNewNativeTransfer(t *testing.T) {
-	builder, _ := builder.NewTxBuilder(xc.NewChainConfig("XYZ").Base())
-	from := xc.Address("from")
-	to := xc.Address("to")
+func TestNewTransfer(t *testing.T) {
+	builder1, _ := builder.NewTxBuilder(xc.NewChainConfig("ICP").Base())
+	from := xc.Address("6c5066261553064a8d4fa8f30fa9d587d9887bce69601cdb5b6cac8780fc8899")
+	to := xc.Address("6c5066261553064a8d4fa8f30fa9d587d9887bce69601cdb5b6cac8780fc8899")
 	amount := xc.AmountBlockchain{}
 	input := &TxInput{}
 	args := buildertest.MustNewTransferArgs(
 		from, to, amount,
+		buildertest.OptionPublicKey([]byte{1, 2, 3, 4}),
 	)
-	_, err := builder.Transfer(args, input)
-	require.ErrorContains(t, err, "not implemented")
+	_, err := builder1.Transfer(args, input)
+	require.NoError(t, err)
 }
 
-func TestNewTokenTransfer(t *testing.T) {
-	builder1, _ := builder.NewTxBuilder(xc.NewChainConfig("XYZ").Base())
-	from := xc.Address("from")
-	to := xc.Address("to")
+func TestNewTransferNoPublicKey(t *testing.T) {
+	builder1, _ := builder.NewTxBuilder(xc.NewChainConfig("ICP").Base())
+	from := xc.Address("6c5066261553064a8d4fa8f30fa9d587d9887bce69601cdb5b6cac8780fc8899")
+	to := xc.Address("6c5066261553064a8d4fa8f30fa9d587d9887bce69601cdb5b6cac8780fc8899")
 	amount := xc.AmountBlockchain{}
 	input := &TxInput{}
 	args := buildertest.MustNewTransferArgs(
@@ -42,5 +43,35 @@ func TestNewTokenTransfer(t *testing.T) {
 		buildertest.OptionContractAddress(xc.ContractAddress("contract")),
 	)
 	_, err := builder1.Transfer(args, input)
-	require.ErrorContains(t, err, "token transfers are not supported for XYZ")
+	require.ErrorContains(t, err, "missing public key")
+}
+
+func TESTNewTokenTransferInvalidContract(t *testing.T) {
+	builder1, _ := builder.NewTxBuilder(xc.NewChainConfig("ICP").Base())
+	from := xc.Address("6c5066261553064a8d4fa8f30fa9d587d9887bce69601cdb5b6cac8780fc8899")
+	to := xc.Address("6c5066261553064a8d4fa8f30fa9d587d9887bce69601cdb5b6cac8780fc8899")
+	amount := xc.AmountBlockchain{}
+	input := &TxInput{}
+	args := buildertest.MustNewTransferArgs(
+		from, to, amount,
+		buildertest.OptionContractAddress(xc.ContractAddress("asdf")),
+		buildertest.OptionPublicKey([]byte{1, 2, 3, 4}),
+	)
+	_, err := builder1.Transfer(args, input)
+	require.NoError(t, err, "failed to decode canister")
+}
+
+func TestNewTokenTransfer(t *testing.T) {
+	builder1, _ := builder.NewTxBuilder(xc.NewChainConfig("ICP").Base())
+	from := xc.Address("mglk4-25zez-he5uh-lsy2a-bontn-pfarj-offxd-5teb2-icnpp-scmni-zae")
+	to := xc.Address("mglk4-25zez-he5uh-lsy2a-bontn-pfarj-offxd-5teb2-icnpp-scmni-zae")
+	amount := xc.AmountBlockchain{}
+	input := &TxInput{}
+	args := buildertest.MustNewTransferArgs(
+		from, to, amount,
+		buildertest.OptionContractAddress(xc.ContractAddress("n5wcd-faaaa-aaaar-qaaea-cai")),
+		buildertest.OptionPublicKey([]byte{1, 2, 3, 4}),
+	)
+	_, err := builder1.Transfer(args, input)
+	require.NoError(t, err, "missing public key")
 }
--- chain/internet_computer/tx/tx_test.go
+++ chain/internet_computer/tx/tx_test.go
@@ -1,30 +1,35 @@
 package tx_test
 
 import (
+	"encoding/hex"
 	"testing"
 
 	xc "github.com/cordialsys/crosschain"
-	"github.com/cordialsys/crosschain/chain/template/tx"
+	"github.com/cordialsys/crosschain/chain/internet_computer/client/types/icp"
+	"github.com/cordialsys/crosschain/chain/internet_computer/tx"
 	"github.com/stretchr/testify/require"
 )
 
 func TestTxHash(t *testing.T) {
-
-	tx1 := tx.Tx{}
-	require.Equal(t, xc.TxHash("not implemented"), tx1.Hash())
-}
-
-func TestTxSighashes(t *testing.T) {
-
-	tx1 := tx.Tx{}
-	sighashes, err := tx1.Sighashes()
-	require.NotNil(t, sighashes)
-	require.EqualError(t, err, "not implemented")
-}
-
-func TestTxAddSignature(t *testing.T) {
-
-	tx1 := tx.Tx{}
-	err := tx1.SetSignatures([]*xc.SignatureResponse{}...)
-	require.EqualError(t, err, "not implemented")
+	pk := "bd08143ec55c47d3be603f8cf395025f8473d0e4d09a72eb83631fc1d745fb31"
+	pkBytes, err := hex.DecodeString(pk)
+	require.NoError(t, err)
+	toStr := "723b365d1ca4bd14f4899cb3d6d028434e3219a9f4de98fa52553dfb6c363af4"
+	to, err := hex.DecodeString(toStr)
+	require.NoError(t, err)
+	ts := icp.NewTimestamp(1_753_737_958_565_528_000)
+	tx1 := tx.Tx{
+		IcrcTransfer: nil,
+		IcpTransfer: &icp.TransferArgs{
+			To:             to,
+			Fee:            icp.NewTokens(10_000),
+			Memo:           0,
+			FromSubaccount: nil,
+			CreatedAtTime:  &ts,
+			Amount:         icp.NewTokens(5_000_000),
+		},
+		Pubkey:   pkBytes,
+		IsIcrcTx: false,
+	}
+	require.Equal(t, xc.TxHash("550322db518f06d67d5ce11df985e2651f553cebfd144eff5473a5d12e47aa1a"), tx1.Hash())
 }
