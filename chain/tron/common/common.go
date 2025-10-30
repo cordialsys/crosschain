ccbf17f63e06e46501b7d13b5d100bd2a8c5fac6
PawelBis <24803993+PawelBis@users.noreply.github.com>
2025-10-30 13:07:58 +0100

    TRON: Add comment about truncating TRX for votes

--- chain/tron/common/common.go
+++ chain/tron/common/common.go
@@ -12,6 +12,7 @@ func VotesToTrx(v uint64, decimals int) xc.AmountBlockchain {
 	return xc.NewAmountBlockchainFromUint64(trxRaw)
 }
 
+// Votes are always a positive natural numbers, we have to truncate the TRX amount.
 func TrxToVotes(trx xc.AmountBlockchain, decimals int) uint64 {
 	d := math.Pow10(decimals)
 	divisor := xc.NewAmountBlockchainFromUint64(uint64(d))
