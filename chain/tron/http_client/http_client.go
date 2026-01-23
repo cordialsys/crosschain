2d82d0039b2588ce7e6f6d8b8497b2e57585eb0e
PawelBis <24803993+PawelBis@users.noreply.github.com>
2026-01-23 11:41:46 +0100

    Reapply "TRX: Implement fee estimation for inclusive fee transfers"
    This reverts commit 91039ce42ae5b7f3691b367bbfc2d26275f24fd2.

--- chain/tron/http_client/http_client.go
+++ chain/tron/http_client/http_client.go
@@ -572,3 +572,81 @@ func (c *Client) GetAccount(address string) (*GetAccountResponse, error) {
 
 	return parsed, nil
 }
+
+type ChainParameter struct {
+	Key   string `json:"key"`
+	Value int    `json:"value"`
+}
+
+type ChainParameters struct {
+	Error
+	Inner []ChainParameter `json:"chainParameter"`
+}
+
+func (params *ChainParameters) GetParam(key string) (int, bool) {
+	for _, p := range params.Inner {
+		if p.Key == key {
+			return p.Value, true
+		}
+	}
+
+	return 0, false
+}
+
+func (c *Client) GetChainParameters() (*ChainParameters, error) {
+	resp, err := c.client.Get(c.Url("wallet/getchainparameters"))
+	if err != nil {
+		return nil, fmt.Errorf("failed to get chain params: %w", err)
+	}
+
+	parsed, err := parseResponse(resp, &ChainParameters{})
+	if err != nil {
+		return nil, err
+	}
+
+	err = checkError(parsed.Error)
+	if err != nil {
+		return parsed, err
+	}
+
+	return parsed, nil
+}
+
+type GetAccountResourcesResponse struct {
+	Error
+	FreeNetLimit int `json:"freeNetLimit"`
+	FreeNetUsed  int `json:"freeNetUsed"`
+	NetLimit     int `json:"NetLimit"`
+	NetUsed      int `json:"NetUsed"`
+}
+
+func (a GetAccountResourcesResponse) GetAvailableBandwith() int {
+	return (a.FreeNetLimit - a.FreeNetUsed) +
+		(a.NetLimit - a.NetUsed)
+}
+
+func (c *Client) GetAccountResources(address string) (*GetAccountResourcesResponse, error) {
+	req, err := postRequest(c.Url("wallet/getaccountresource"), map[string]interface{}{
+		"address": address,
+		"visible": true,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	parsed, err := parseResponse(resp, &GetAccountResourcesResponse{})
+	if err != nil {
+		return nil, err
+	}
+
+	err = checkError(parsed.Error)
+	if err != nil {
+		return parsed, err
+	}
+
+	return parsed, nil
+}
