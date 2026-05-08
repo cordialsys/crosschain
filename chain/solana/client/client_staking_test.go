4a2f87012bd00850784e3047690cec9f6e6602e1
Conor Patrick <conor@cordialsystems.com>
2026-05-08 11:53:00 -0400

    solana: dont setup durable nonce when remaining balance is below rent

--- chain/solana/client/client_staking_test.go
+++ chain/solana/client/client_staking_test.go
@@ -42,16 +42,11 @@ func TestFetchStakingInput(t *testing.T) {
 	}{
 		{
 			description: "get staking info",
-			resp: []string{
-				// valid blockhash
-				`{"context":{"slot":83986105},"value":{"blockhash":"DvLEyV2GHk86K5GojpqnRsvhfMF5kdZomKMnhVpvHyqK","feeCalculator":{"lamportsPerSignature":5000}}}`,
-				// nonce account not found
-				`{"context":{"apiVersion":"2.0.5","slot":83986105},"value":null}`,
-				// rent exemption for nonce account creation
-				`2039280`,
+			resp: solanaBaseInputResponses(
+				solanaValidBlockhashResponse,
 				// vote account list
 				`{"jsonrpc":"2.0","result":{"current":[{"activatedStake":41061582618205,"commission":7,"epochCredits":[[645,196752727,196348579],[646,197152299,196752727],[647,197549513,197152299],[648,197955321,197549513],[649,198362807,197955321]],"epochVoteAccount":true,"lastVote":280799357,"nodePubkey":"CVAAQGA8GBzKi4kLdmpDuJnpkSik6PMWSvRk3RDds9K8","rootSlot":280799326,"votePubkey":"XBtfuT5gYU27UAukT3pEzgiKgHpHNQhSoa3zX2PYtiT"},{"activatedStake":32347208647108,"commission":7,"epochCredits":[[645,33158933,32754517],[646,33548343,33158933],[647,33944043,33548343],[648,34350430,33944043],[649,34758214,34350430]],"epochVoteAccount":true,"lastVote":280799357,"nodePubkey":"EqgfgrWR3D1As2aS7tYjoHfNxgxcfNYvdUL5zCsXFXBt","rootSlot":280799326,"votePubkey":"3m8Ct5n9feJFEuuXFb67oqt9XEJeBYkGyEdQRX33QQ5H"}]}}`,
-			},
+			),
 			validator: "3m8Ct5n9feJFEuuXFb67oqt9XEJeBYkGyEdQRX33QQ5H",
 			expected: &tx_input.StakingInput{
 				TxInput: tx_input.TxInput{
@@ -68,16 +63,11 @@ func TestFetchStakingInput(t *testing.T) {
 		{
 			// we need the validator vote account, but we can identify it by the validator identity
 			description: "get staking info by validator identity",
-			resp: []string{
-				// valid blockhash
-				`{"context":{"slot":83986105},"value":{"blockhash":"DvLEyV2GHk86K5GojpqnRsvhfMF5kdZomKMnhVpvHyqK","feeCalculator":{"lamportsPerSignature":5000}}}`,
-				// nonce account not found
-				`{"context":{"apiVersion":"2.0.5","slot":83986105},"value":null}`,
-				// rent exemption for nonce account creation
-				`2039280`,
+			resp: solanaBaseInputResponses(
+				solanaValidBlockhashResponse,
 				// vote account list
 				`{"jsonrpc":"2.0","result":{"current":[{"activatedStake":41061582618205,"commission":7,"epochCredits":[[645,196752727,196348579],[646,197152299,196752727],[647,197549513,197152299],[648,197955321,197549513],[649,198362807,197955321]],"epochVoteAccount":true,"lastVote":280799357,"nodePubkey":"CVAAQGA8GBzKi4kLdmpDuJnpkSik6PMWSvRk3RDds9K8","rootSlot":280799326,"votePubkey":"XBtfuT5gYU27UAukT3pEzgiKgHpHNQhSoa3zX2PYtiT"},{"activatedStake":32347208647108,"commission":7,"epochCredits":[[645,33158933,32754517],[646,33548343,33158933],[647,33944043,33548343],[648,34350430,33944043],[649,34758214,34350430]],"epochVoteAccount":true,"lastVote":280799357,"nodePubkey":"EqgfgrWR3D1As2aS7tYjoHfNxgxcfNYvdUL5zCsXFXBt","rootSlot":280799326,"votePubkey":"3m8Ct5n9feJFEuuXFb67oqt9XEJeBYkGyEdQRX33QQ5H"}]}}`,
-			},
+			),
 			validator: "EqgfgrWR3D1As2aS7tYjoHfNxgxcfNYvdUL5zCsXFXBt",
 			expected: &tx_input.StakingInput{
 				TxInput: tx_input.TxInput{
@@ -94,16 +84,11 @@ func TestFetchStakingInput(t *testing.T) {
 		{
 			// we need the validator vote account, but we can identify it by the validator identity
 			description: "invalid validator",
-			resp: []string{
-				// valid blockhash
-				`{"context":{"slot":83986105},"value":{"blockhash":"DvLEyV2GHk86K5GojpqnRsvhfMF5kdZomKMnhVpvHyqK","feeCalculator":{"lamportsPerSignature":5000}}}`,
-				// nonce account not found
-				`{"context":{"apiVersion":"2.0.5","slot":83986105},"value":null}`,
-				// rent exemption for nonce account creation
-				`2039280`,
+			resp: solanaBaseInputResponses(
+				solanaValidBlockhashResponse,
 				// vote account list
 				`{"jsonrpc":"2.0","result":{"current":[{"activatedStake":41061582618205,"commission":7,"epochCredits":[[645,196752727,196348579],[646,197152299,196752727],[647,197549513,197152299],[648,197955321,197549513],[649,198362807,197955321]],"epochVoteAccount":true,"lastVote":280799357,"nodePubkey":"CVAAQGA8GBzKi4kLdmpDuJnpkSik6PMWSvRk3RDds9K8","rootSlot":280799326,"votePubkey":"XBtfuT5gYU27UAukT3pEzgiKgHpHNQhSoa3zX2PYtiT"},{"activatedStake":32347208647108,"commission":7,"epochCredits":[[645,33158933,32754517],[646,33548343,33158933],[647,33944043,33548343],[648,34350430,33944043],[649,34758214,34350430]],"epochVoteAccount":true,"lastVote":280799357,"nodePubkey":"EqgfgrWR3D1As2aS7tYjoHfNxgxcfNYvdUL5zCsXFXBt","rootSlot":280799326,"votePubkey":"3m8Ct5n9feJFEuuXFb67oqt9XEJeBYkGyEdQRX33QQ5H"}]}}`,
-			},
+			),
 			validator: "o7hZ7ceQYKTXgwJdEkczQmZmcrZTszmiTw1K1sPEaYn",
 			err:       "validator vote account not found",
 		},
@@ -181,18 +166,13 @@ func TestFetchUnstakingInput(t *testing.T) {
 	}{
 		{
 			description: "get unstaking info",
-			resp: []string{
+			resp: solanaResponsesWithBaseInput([]string{
 				// stake accounts
 				`[{"account":{"data":{"parsed":{"info":{"meta":{"authorized":{"staker":"83wDqn8DFg5oh1WetQJwcyZySjxGkxWVKf3p39T6GMQH","withdrawer":"83wDqn8DFg5oh1WetQJwcyZySjxGkxWVKf3p39T6GMQH"},"lockup":{"custodian":"11111111111111111111111111111111","epoch":0,"unixTimestamp":0},"rentExemptReserve":"2282880"},"stake":{"creditsObserved":101316504,"delegation":{"activationEpoch":"650","deactivationEpoch":"650","stake":"7717120","voter":"J2nUHEAgZFRyuJbFjdqPrAa9gyWDuc7hErtDQHPhsYRp","warmupCooldownRate":0.25}}},"type":"delegated"},"program":"stake","space":200},"executable":false,"lamports":10000000,"owner":"Stake11111111111111111111111111111111111111","rentEpoch":18446744073709552000,"space":200},"pubkey":"GuXr1c5KyuJxpsoKMDiDBAJZq4GczPMNUmp4UKY9LbAE"},{"account":{"data":{"parsed":{"info":{"meta":{"authorized":{"staker":"83wDqn8DFg5oh1WetQJwcyZySjxGkxWVKf3p39T6GMQH","withdrawer":"83wDqn8DFg5oh1WetQJwcyZySjxGkxWVKf3p39T6GMQH"},"lockup":{"custodian":"11111111111111111111111111111111","epoch":0,"unixTimestamp":0},"rentExemptReserve":"2282880"},"stake":{"creditsObserved":101731298,"delegation":{"activationEpoch":"650","deactivationEpoch":"18446744073709551615","stake":"37731751","voter":"J2nUHEAgZFRyuJbFjdqPrAa9gyWDuc7hErtDQHPhsYRp","warmupCooldownRate":0.25}}},"type":"delegated"},"program":"stake","space":200},"executable":false,"lamports":40016458,"owner":"Stake11111111111111111111111111111111111111","rentEpoch":18446744073709552000,"space":200},"pubkey":"6LFjBX1yUwSr8SWsyZUc5okZiVo8ZdmVQ9keJAazRmnh"},{"account":{"data":{"parsed":{"info":{"meta":{"authorized":{"staker":"83wDqn8DFg5oh1WetQJwcyZySjxGkxWVKf3p39T6GMQH","withdrawer":"83wDqn8DFg5oh1WetQJwcyZySjxGkxWVKf3p39T6GMQH"},"lockup":{"custodian":"11111111111111111111111111111111","epoch":0,"unixTimestamp":0},"rentExemptReserve":"2282880"},"stake":{"creditsObserved":101316504,"delegation":{"activationEpoch":"649","deactivationEpoch":"650","stake":"717400","voter":"J2nUHEAgZFRyuJbFjdqPrAa9gyWDuc7hErtDQHPhsYRp","warmupCooldownRate":0.25}}},"type":"delegated"},"program":"stake","space":200},"executable":false,"lamports":3000322,"owner":"Stake11111111111111111111111111111111111111","rentEpoch":18446744073709552000,"space":200},"pubkey":"8zrSGLMdE6dK57Q7a8N8TDohmyft1MrsLYdRqhDvCerc"},{"account":{"data":{"parsed":{"info":{"meta":{"authorized":{"staker":"83wDqn8DFg5oh1WetQJwcyZySjxGkxWVKf3p39T6GMQH","withdrawer":"83wDqn8DFg5oh1WetQJwcyZySjxGkxWVKf3p39T6GMQH"},"lockup":{"custodian":"11111111111111111111111111111111","epoch":0,"unixTimestamp":0},"rentExemptReserve":"2282880"},"stake":{"creditsObserved":102020250,"delegation":{"activationEpoch":"652","deactivationEpoch":"18446744073709551615","stake":"7717120","voter":"J2nUHEAgZFRyuJbFjdqPrAa9gyWDuc7hErtDQHPhsYRp","warmupCooldownRate":0.25}}},"type":"delegated"},"program":"stake","space":200},"executable":false,"lamports":10000000,"owner":"Stake11111111111111111111111111111111111111","rentEpoch":18446744073709552000,"space":200},"pubkey":"BYoo5izmpyrkc4fKkJy2gp6Bwc9evt4vgCYYMY3NHu9C"},{"account":{"data":{"parsed":{"info":{"meta":{"authorized":{"staker":"83wDqn8DFg5oh1WetQJwcyZySjxGkxWVKf3p39T6GMQH","withdrawer":"83wDqn8DFg5oh1WetQJwcyZySjxGkxWVKf3p39T6GMQH"},"lockup":{"custodian":"11111111111111111111111111111111","epoch":0,"unixTimestamp":0},"rentExemptReserve":"2282880"},"stake":{"creditsObserved":101731298,"delegation":{"activationEpoch":"650","deactivationEpoch":"18446744073709551615","stake":"1717786","voter":"J2nUHEAgZFRyuJbFjdqPrAa9gyWDuc7hErtDQHPhsYRp","warmupCooldownRate":0.25}}},"type":"delegated"},"program":"stake","space":200},"executable":false,"lamports":4000749,"owner":"Stake11111111111111111111111111111111111111","rentEpoch":18446744073709552000,"space":200},"pubkey":"CCTFhyxoUHGmdQvuUxFquyYMK4H5hdqwCCN7XAXtK9HC"}]`,
-				// valid blockhash
-				`{"context":{"slot":83986105},"value":{"blockhash":"DvLEyV2GHk86K5GojpqnRsvhfMF5kdZomKMnhVpvHyqK","feeCalculator":{"lamportsPerSignature":5000}}}`,
-				// nonce account not found
-				`{"context":{"apiVersion":"2.0.5","slot":83986105},"value":null}`,
-				// rent exemption for nonce account creation
-				`2039280`,
+			}, solanaValidBlockhashResponse,
 				// epoch info
 				`{"absoluteSlot": 166598,"blockHeight": 166500,"epoch": 650,"slotIndex": 2790,"slotsInEpoch": 8192,"transactionCount": 22661093}`,
-			},
+			),
 			validator: "J2nUHEAgZFRyuJbFjdqPrAa9gyWDuc7hErtDQHPhsYRp",
 			expected: &tx_input.UnstakingInput{
 				TxInput: tx_input.TxInput{
@@ -287,18 +267,13 @@ func TestFetchWithdrawInput(t *testing.T) {
 	}{
 		{
 			description: "get withdraw info",
-			resp: []string{
+			resp: solanaResponsesWithBaseInput([]string{
 				// stake accounts
 				`[{"account":{"data":{"parsed":{"info":{"meta":{"authorized":{"staker":"83wDqn8DFg5oh1WetQJwcyZySjxGkxWVKf3p39T6GMQH","withdrawer":"83wDqn8DFg5oh1WetQJwcyZySjxGkxWVKf3p39T6GMQH"},"lockup":{"custodian":"11111111111111111111111111111111","epoch":0,"unixTimestamp":0},"rentExemptReserve":"2282880"},"stake":{"creditsObserved":102020250,"delegation":{"activationEpoch":"652","deactivationEpoch":"18446744073709551615","stake":"7717120","voter":"J2nUHEAgZFRyuJbFjdqPrAa9gyWDuc7hErtDQHPhsYRp","warmupCooldownRate":0.25}}},"type":"delegated"},"program":"stake","space":200},"executable":false,"lamports":10000000,"owner":"Stake11111111111111111111111111111111111111","rentEpoch":18446744073709552000,"space":200},"pubkey":"BYoo5izmpyrkc4fKkJy2gp6Bwc9evt4vgCYYMY3NHu9C"},{"account":{"data":{"parsed":{"info":{"meta":{"authorized":{"staker":"83wDqn8DFg5oh1WetQJwcyZySjxGkxWVKf3p39T6GMQH","withdrawer":"83wDqn8DFg5oh1WetQJwcyZySjxGkxWVKf3p39T6GMQH"},"lockup":{"custodian":"11111111111111111111111111111111","epoch":0,"unixTimestamp":0},"rentExemptReserve":"2282880"},"stake":{"creditsObserved":101731298,"delegation":{"activationEpoch":"650","deactivationEpoch":"18446744073709551615","stake":"27727872","voter":"J2nUHEAgZFRyuJbFjdqPrAa9gyWDuc7hErtDQHPhsYRp","warmupCooldownRate":0.25}}},"type":"delegated"},"program":"stake","space":200},"executable":false,"lamports":30012094,"owner":"Stake11111111111111111111111111111111111111","rentEpoch":18446744073709552000,"space":200},"pubkey":"GSQJ1PmGtY11efVjmEuUyim4PqXKsB7tnPp1jvpoFeRz"},{"account":{"data":{"parsed":{"info":{"meta":{"authorized":{"staker":"83wDqn8DFg5oh1WetQJwcyZySjxGkxWVKf3p39T6GMQH","withdrawer":"83wDqn8DFg5oh1WetQJwcyZySjxGkxWVKf3p39T6GMQH"},"lockup":{"custodian":"11111111111111111111111111111111","epoch":0,"unixTimestamp":0},"rentExemptReserve":"2282880"},"stake":{"creditsObserved":101316504,"delegation":{"activationEpoch":"650","deactivationEpoch":"650","stake":"7717120","voter":"J2nUHEAgZFRyuJbFjdqPrAa9gyWDuc7hErtDQHPhsYRp","warmupCooldownRate":0.25}}},"type":"delegated"},"program":"stake","space":200},"executable":false,"lamports":10000000,"owner":"Stake11111111111111111111111111111111111111","rentEpoch":18446744073709552000,"space":200},"pubkey":"GuXr1c5KyuJxpsoKMDiDBAJZq4GczPMNUmp4UKY9LbAE"},{"account":{"data":{"parsed":{"info":{"meta":{"authorized":{"staker":"83wDqn8DFg5oh1WetQJwcyZySjxGkxWVKf3p39T6GMQH","withdrawer":"83wDqn8DFg5oh1WetQJwcyZySjxGkxWVKf3p39T6GMQH"},"lockup":{"custodian":"11111111111111111111111111111111","epoch":0,"unixTimestamp":0},"rentExemptReserve":"2282880"},"stake":{"creditsObserved":101731298,"delegation":{"activationEpoch":"650","deactivationEpoch":"18446744073709551615","stake":"17723993","voter":"J2nUHEAgZFRyuJbFjdqPrAa9gyWDuc7hErtDQHPhsYRp","warmupCooldownRate":0.25}}},"type":"delegated"},"program":"stake","space":200},"executable":false,"lamports":20007731,"owner":"Stake11111111111111111111111111111111111111","rentEpoch":18446744073709552000,"space":200},"pubkey":"FXw5CT4CeyZoBd5Nzqad2CoPUxSwJhx23dDkhxq4sDHs"},{"account":{"data":{"parsed":{"info":{"meta":{"authorized":{"staker":"83wDqn8DFg5oh1WetQJwcyZySjxGkxWVKf3p39T6GMQH","withdrawer":"83wDqn8DFg5oh1WetQJwcyZySjxGkxWVKf3p39T6GMQH"},"lockup":{"custodian":"11111111111111111111111111111111","epoch":0,"unixTimestamp":0},"rentExemptReserve":"2282880"},"stake":{"creditsObserved":101731298,"delegation":{"activationEpoch":"650","deactivationEpoch":"18446744073709551615","stake":"37731751","voter":"J2nUHEAgZFRyuJbFjdqPrAa9gyWDuc7hErtDQHPhsYRp","warmupCooldownRate":0.25}}},"type":"delegated"},"program":"stake","space":200},"executable":false,"lamports":40016458,"owner":"Stake11111111111111111111111111111111111111","rentEpoch":18446744073709552000,"space":200},"pubkey":"6LFjBX1yUwSr8SWsyZUc5okZiVo8ZdmVQ9keJAazRmnh"},{"account":{"data":{"parsed":{"info":{"meta":{"authorized":{"staker":"83wDqn8DFg5oh1WetQJwcyZySjxGkxWVKf3p39T6GMQH","withdrawer":"83wDqn8DFg5oh1WetQJwcyZySjxGkxWVKf3p39T6GMQH"},"lockup":{"custodian":"11111111111111111111111111111111","epoch":0,"unixTimestamp":0},"rentExemptReserve":"2282880"},"stake":{"creditsObserved":101316504,"delegation":{"activationEpoch":"649","deactivationEpoch":"650","stake":"717400","voter":"J2nUHEAgZFRyuJbFjdqPrAa9gyWDuc7hErtDQHPhsYRp","warmupCooldownRate":0.25}}},"type":"delegated"},"program":"stake","space":200},"executable":false,"lamports":3000322,"owner":"Stake11111111111111111111111111111111111111","rentEpoch":18446744073709552000,"space":200},"pubkey":"8zrSGLMdE6dK57Q7a8N8TDohmyft1MrsLYdRqhDvCerc"},{"account":{"data":{"parsed":{"info":{"meta":{"authorized":{"staker":"83wDqn8DFg5oh1WetQJwcyZySjxGkxWVKf3p39T6GMQH","withdrawer":"83wDqn8DFg5oh1WetQJwcyZySjxGkxWVKf3p39T6GMQH"},"lockup":{"custodian":"11111111111111111111111111111111","epoch":0,"unixTimestamp":0},"rentExemptReserve":"2282880"},"stake":{"creditsObserved":101731298,"delegation":{"activationEpoch":"650","deactivationEpoch":"18446744073709551615","stake":"1717786","voter":"J2nUHEAgZFRyuJbFjdqPrAa9gyWDuc7hErtDQHPhsYRp","warmupCooldownRate":0.25}}},"type":"delegated"},"program":"stake","space":200},"executable":false,"lamports":4000749,"owner":"Stake11111111111111111111111111111111111111","rentEpoch":18446744073709552000,"space":200},"pubkey":"CCTFhyxoUHGmdQvuUxFquyYMK4H5hdqwCCN7XAXtK9HC"}]`,
-				// valid blockhash
-				`{"context":{"slot":83986105},"value":{"blockhash":"DvLEyV2GHk86K5GojpqnRsvhfMF5kdZomKMnhVpvHyqK","feeCalculator":{"lamportsPerSignature":5000}}}`,
-				// nonce account not found
-				`{"context":{"apiVersion":"2.0.5","slot":83986105},"value":null}`,
-				// rent exemption for nonce account creation
-				`2039280`,
+			}, solanaValidBlockhashResponse,
 				// epoch info
 				`{"absoluteSlot": 166598,"blockHeight": 166500,"epoch": 652,"slotIndex": 2790,"slotsInEpoch": 8192,"transactionCount": 22661093}`,
-			},
+			),
 			validator: "J2nUHEAgZFRyuJbFjdqPrAa9gyWDuc7hErtDQHPhsYRp",
 			expected: &tx_input.WithdrawInput{
 				TxInput: tx_input.TxInput{
